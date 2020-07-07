7f5bc83a0114ec2efd902c4145ecbf58e20eecd0
Paulo Henrique <paulotux.mendes@gmail.com>
2020-07-07 17:06:08 -0500

    [UPDATE] Criando o Dockerfile para o projeto

---
+++ docker/entrypoint/local-dev.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+./docker/sh/porcentage.sh
+echo "Access entrypoint"
+set -e
+echo "[ ${PORCENTAGE_0} ] Back - Starting Endpoint of Application"
+
+if  ! [ -e "/var/www/html/config.php" ] ; then
+    echo "[ ****************** ] Copying sample application configuration to real one"
+    cp /var/www/html/example-config.php /var/www/html/config.php
+
+    # Regex to many "/"
+    sed -i "s@HOME_URI_VAL@$HOME_URI_VAL@g" /var/www/html/config.php
+
+    sed -i "s/@@DURACAO_TOKEN@@/$DURACAO_TOKEN/g" /var/www/html/config.php
+    sed -i "s/@@HOSTNAME@@/$HOSTNAME/g" /var/www/html/config.php
+    sed -i "s/@@DB_NAME@@/$DB_NAME/g" /var/www/html/config.php
+    sed -i "s/@@DB_USER@@/$DB_USER/g" /var/www/html/config.php
+    sed -i "s/@@DB_PASSWORD@@/$DB_PASSWORD/g" /var/www/html/config.php
+    sed -i "s/@@DB_PORT@@/$DB_PORT/g" /var/www/html/config.php
+    sed -i "s/@@DB_CHARSET@@/$DB_CHARSET/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_HOSTNAME@@/$MAIL_HOSTNAME/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_USER@@/$MAIL_USER/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_PASSWORD@@/$MAIL_PASSWORD/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_PORT@@/$MAIL_PORT/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_FROM@@/$MAIL_FROM/g" /var/www/html/config.php
+    sed -i "s/@@MAIL_FROM_NAME@@/$MAIL_FROM_NAME/g" /var/www/html/config.php
+
+    sed -i "s/@@DEBUG@@/$DEBUG/g" /var/www/html/config.php
+fi
+
+ if ! [ -d "./vendor" ]
+  then
+    echo "[ ${PORCENTAGE_10} ] Install depedences whit composer..."
+    # composer install --ignore-platform-reqs --verbose --no-scripts
+ fi
+
+apache2-foreground
+echo "[ ${PORCENTAGE_100} ] Back - Ending Endpoint of Application"
---
+++ example-config.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Configuração geral
+ */
+// Caminho para a raiz
+define( 'ABSPATH', dirname( __FILE__ ) );
+
+// Caminho para a pasta de uploads
+define( 'UP_ABSPATH', ABSPATH . '/views/_uploads' );
+
+// URL da home
+define( 'HOME_URI', 'HOME_URI_VAL');
+
+// Duração de vida do Token por dia
+define( 'DURACAO_TOKEN', '@@DURACAO_TOKEN@@' );
+
+
+/** DB properties */
+// Nome do host da base de dados
+define( 'HOSTNAME', '@@HOSTNAME@@' );
+
+// Nome do banco de dados
+define( 'DB_NAME', '@@DB_NAME@@' );
+
+// Usuário da base de dados
+define( 'DB_USER', '@@DB_USER@@' );
+
+// Senha do usuário da base de dados
+define( 'DB_PASSWORD', '@@DB_PASSWORD@@' );
+
+//port do DB
+define( 'DB_PORT', '5432' );
+
+// Charset da conexão PDO
+define( 'DB_CHARSET', 'utf8' );
+/** Fim - DB properties */
+
+/** Mail properties */
+// Endereço do servidor SMTP
+define( 'MAIL_HOSTNAME', '@@MAIL_HOSTNAME@@' );
+
+// Usuário do servidor SMTP
+define( 'MAIL_USER', '@@MAIL_USER@@' );
+
+//Senha do servidor SMTP
+define( 'MAIL_PASSWORD', '@@MAIL_PASSWORD@@' );
+
+//port do MAIL - 587
+define( 'MAIL_PORT', '@@MAIL_PORT@@' );
+
+// Define o remetente email
+define( 'MAIL_FROM', '@@MAIL_FROM@@' );
+
+// Define o remetente nome
+define( 'MAIL_FROM_NAME', '@@MAIL_FROM_NAME@@' );
+/** Fim - MAIL properties */
+
+// Se você estiver desenvolvendo, modifique o valor para true
+define( 'DEBUG', '@@DEBUG@@' );
+
+/**
+ * Não edite daqui em diante
+ */
+
+// Carrega o loader, que vai carregar a aplicação inteira
+require_once ABSPATH . '/loader.php';
